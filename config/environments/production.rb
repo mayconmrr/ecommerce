cfb371b2f9dc8498ef80cbae004062c25c432b2c
mayconmrr <maycon.mrr@gmail.com>
2017-07-04 11:56:26 -0300

    region aws

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -83,6 +83,7 @@ Rails.application.configure do
   :access_key_id => ENV['AWS_ACCESS_KEY_ID'],
   :secret_access_key => ENV['AWS_SECRET_ACCESS_KEY']
   },
-  :s3_host_name => "s3-us-west-2.amazonaws.com"  # Added entry
+  :s3_host_name => "s3-us-west-2.amazonaws.com",  # Added entry
+  :s3_region => 'us-west-2'
   }
 end
